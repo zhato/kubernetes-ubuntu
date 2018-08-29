d4f8f5a4234d421f5d31a0b579596e4e1532aea9
unknown <inhyun.ko@gmail.com>
2018-08-29 21:47:44 +0900

    swapoff

--- common.sh
+++ common.sh
@@ -17,3 +17,8 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
 apt update
 apt install -y kubelet kubeadm kubectl
+
+# kubelet requires swap off
+swapoff -a
+# keep swap off after reboot
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
