387b206b92a0650747924f9a7d3e464bd28d1586
unknown <inhyun.ko@gmail.com>
2018-10-01 23:45:44 +0900

    code optimize

--- common.sh
+++ common.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 # 2018-06-15
 
-apt upgrade -y
+# Common
+apt remove docker docker-engine docker.io
+apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 # Docker
-apt remove docker docker-engine docker.io
-apt update
-apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update && apt install -y docker-ce
 
 # Kubernetes
-apt update && apt install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
-apt update
-apt install -y kubelet kubeadm kubectl
+apt update && apt install -y kubelet kubeadm kubectl
 
 # kubelet requires swap off
 swapoff -a
