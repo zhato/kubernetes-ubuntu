5b2c4ca10006270c3b1454e3520512085e741cd2
unknown <inhyun.ko@gmail.com>
2018-10-02 00:01:39 +0900

    CRLF to LF

--- common.sh
+++ common.sh
@@ -2,20 +2,20 @@
 # 2018-06-15
 
 # Common
-sudo apt remove docker docker-engine docker.io
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+apt remove -y docker docker-engine docker.io
+apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 # Docker
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update && apt install -y docker-ce
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update && apt install -y docker-ce
 
 # Kubernetes
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
-sudo apt update && apt install -y kubelet kubeadm kubectl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
+apt update && apt install -y kubelet kubeadm kubectl
 
 # kubelet requires swap off
-sudo swapoff -a
+swapoff -a
 # keep swap off after reboot
-sudo sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
--- node.sh
+++ node.sh
@@ -5,8 +5,8 @@ MASTERIP=$1
 TOKEN=$2
 NODEIP=$3
 SEQ=$4
-DISCOVERY_TOKEN=$(sudo grep "  kubeadm join" /shared/kubeadm.out | awk '{print $NF}')
+DISCOVERY_TOKEN=$(grep "  kubeadm join" /shared/kubeadm.out | awk '{print $NF}')
 
-sudo sed "s/127.0.0.1.*node-$SEQ/$NODEIP node-$SEQ/" -i /etc/hosts
+sed "s/127.0.0.1.*node-$SEQ/$NODEIP node-$SEQ/" -i /etc/hosts
 
-sudo kubeadm join --node-name `hostname` --token $TOKEN $MASTERIP:8080 --discovery-token-ca-cert-hash $DISCOVERY_TOKEN
+kubeadm join --node-name `hostname` --token $TOKEN $MASTERIP:8080 --discovery-token-ca-cert-hash $DISCOVERY_TOKEN
